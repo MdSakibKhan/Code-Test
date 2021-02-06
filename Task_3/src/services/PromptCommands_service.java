b387c1e2bdbcbfd579c07cd3ba7fe19893c304f2
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-02-06 21:55:27 +0600

    Delete Task_3 directory

--- Task_3/src/services/PromptCommands_service.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-public class PromptCommands_service {
-    public void print(){
-        System.out.println("A. Create Routine");
-        System.out.println("B. Show Routine");
-        System.out.println("C. List Courses with Teachers Name");
-    }
-}
