da5ffae88738e12433885d2c0bc4132678904640
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-02-07 22:29:08 +0600

    Update service_B.java

--- Task_3/services/service_B.java
+++ Task_3/services/service_B.java
@@ -6,6 +6,10 @@ import repository.Store;
 public class service_B {
 
     public void task(Store mainStore) {
+        if (mainStore.getClassPlan_items().size()==0){
+            System.out.println("No Class Plan Recorded Yet.");
+            return;
+        }
         for(ClassPlan cls : mainStore.getClassPlan_items()) {
             System.out.println(cls.getInfo());
         }
