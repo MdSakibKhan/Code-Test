c604dd5e964e689a70fad7779f835d3aafaadae6
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-02-08 12:52:07 +0600

    Update teacher.java

--- Task_3/model/teacher.java
+++ Task_3/model/teacher.java
@@ -4,6 +4,7 @@ public class teacher {
     private int id;
     private String name = "";
     public teacher(int id, String name){
+        this.id = id;
         this.name = name;
     }
     public String getName() {
